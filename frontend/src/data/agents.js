d178f444c80cf378130e67e70433dcf81881a7cd
swidom <u7834930@anu.edu.au>
2026-03-13 17:09:38 +1100

    Upload profile picture

--- frontend/src/data/agents.js
+++ frontend/src/data/agents.js
@@ -1,12 +1,21 @@
+import jettImg from "../assets/jett.png";
+import razeImg from "../assets/raze.png";
+import omenImg from "../assets/omen.png";
+import brimstoneImg from "../assets/brimstone.png";
+import sovaImg from "../assets/sova.png";
+import skyeImg from "../assets/skye.png";
+import killjoyImg from "../assets/killjoy.png";
+import cypherImg from "../assets/cypher.png";
+
 export const agentData = [
-    { name: "Jett", role: "Duelist" },
-    { name: "Raze", role: "Duelist" },
-    { name: "Omen", role: "Controller" },
-    { name: "Brimstone", role: "Controller" },
-    { name: "Sova", role: "Initiator" },
-    { name: "Skye", role: "Initiator" },
-    { name: "Killjoy", role: "Sentinel" },
-    { name: "Cypher", role: "Sentinel" }
+    { name: "Jett", role: "Duelist", image: jettImg },
+    { name: "Raze", role: "Duelist", image: razeImg },
+    { name: "Omen", role: "Controller", image: omenImg },
+    { name: "Brimstone", role: "Controller", image: brimstoneImg },
+    { name: "Sova", role: "Initiator", image: sovaImg },
+    { name: "Skye", role: "Initiator", image: skyeImg },
+    { name: "Killjoy", role: "Sentinel", image: killjoyImg },
+    { name: "Cypher", role: "Sentinel", image: cypherImg },
 ];
 
 export const maps = ["Ascent", "Bind", "Split", "Haven"];
